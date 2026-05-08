50cad108fc71cb4d9cff02f2f24c638e95653d97
roshan-2501 <roshant2513@gmail.com>
2026-05-08 16:22:38 +0530

    Initial microservices project setup

---
+++ auth-service/src/test/java/com/projectManagentTool/auth_service/AuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projectManagentTool.auth_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
